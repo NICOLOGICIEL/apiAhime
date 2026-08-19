72765acfafa62a44d53f5917293d53292a1c3cb1
NICOLOGICIEL <kouassister@gmail.com>
2026-08-19 15:26:59 +0000

    Fix APP_URL and add CORS headers to index.php

--- public/index.php
+++ public/index.php
@@ -12,6 +12,15 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 $app = require __DIR__.'/../bootstrap/app.php';
 
 $app->run();
